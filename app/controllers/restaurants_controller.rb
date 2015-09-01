12f460a1821fbcb189fcfc3a2625fa44d76fbb03
Ajitsy <sanghera.p@gmail.com>
2015-09-01 14:59:46 +0100

    fill out form and add new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,17 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+     @restaurant = Restaurant.new
+   end
+
+   def restaurant_params
+     params.require(:restaurant).permit(:name)
+   end
+
+   def create
+     @restaurant = Restaurant.create(restaurant_params)
+     redirect_to '/restaurants'
+   end
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -10,14 +10,26 @@ feature 'restaurants' do
   end
 
   context 'restaurants have been added' do
-  before do
-    Restaurant.create(name: 'KFC')
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
+    end
   end
 
-  scenario 'display restaurants' do
-    visit '/restaurants'
-    expect(page).to have_content('KFC')
-    expect(page).not_to have_content('No restaurants yet')
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
   end
-end
+
 end
